6bd9c4fe8e2075094df88306e47c22b09a72e7b4
charchil08 <er.charchilkajaliya@gmail.com>
2023-11-13 14:33:31 +0530

    parser to text

--- PdfExtractor/Controllers/FileController.cs
+++ PdfExtractor/Controllers/FileController.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PdfExtractor.Controllers;
@@ -14,42 +18,78 @@ public class FileController : ControllerBase
     }
 
     [HttpPost("upload")]
-public string Upload(IFormFileCollection files)
-{
-    try
+    public IList<string> Upload(IFormFileCollection files)
     {
-        // Generate a unique folder name for the current request
-        string uniqueDirName = $"Request_{Guid.NewGuid()}";
-
-        // Construct the folder path
-        var uniqueDirPath = Path.Combine(_hostEnvironment.WebRootPath, uniqueDirName);
-
-        // Check if the folder already exists; create it if not
-        if (!Directory.Exists(uniqueDirPath))
+        IList<string> extractedTexts = new List<string>();
+        try
         {
-            Directory.CreateDirectory(uniqueDirPath);
-        }
+            // Generate a unique folder name for the current request
+            string uniqueDirName = $"Request_{Guid.NewGuid()}";
 
-        foreach (var file in files)
-        {
-            if (file.Length > 0)
+            // Construct the folder path
+            var uniqueDirPath = Path.Combine(_hostEnvironment.WebRootPath, uniqueDirName);
+
+            // Check if the folder already exists; create it if not
+            if (!Directory.Exists(uniqueDirPath))
             {
-                // Construct the file path within the unique folder
-                var filePath = Path.Combine(uniqueDirPath, file.FileName);
+                Directory.CreateDirectory(uniqueDirPath);
+            }
 
-                using var stream = new FileStream(filePath, FileMode.Create);
-                file.CopyTo(stream);
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    // Construct the file path within the unique folder
+                    var filePath = Path.Combine(uniqueDirPath, file.FileName);
 
-                Console.WriteLine($"{file.FileName}-{file.ContentType} is uploaded successfully!");
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    extractedTexts.Add(ExtractTextFromPDF(filePath));
+                    Console.WriteLine($"{file.FileName}-{file.ContentType} is uploaded successfully!");
+                }
+            }
+
+            return extractedTexts;
+        }
+        catch (Exception ex)
+        {
+            return new List<string>() { $"Exception occurred ! - {ex.Message}" };
+        }
+    }
+
+
+    private static string ExtractTextFromPDF(string filePath)
+    {
+        var extractedText = new StringBuilder();
+
+        try
+        {
+            using (PdfDocument pdfDocument = new PdfDocument(new PdfReader(filePath)))
+            {
+                int numPages = pdfDocument.GetNumberOfPages();
+                for (int pageNum = 1; pageNum <= numPages; pageNum++)
+                {
+                    PdfPage page = pdfDocument.GetPage(pageNum);
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+
+                    // Extract text from the page
+                    PdfCanvasProcessor parser = new PdfCanvasProcessor(strategy);
+                    parser.ProcessPageContent(page);
+
+                    // Append the page text to the result using StringBuilder
+                    extractedText.Append(strategy.GetResultantText());
+                }
             }
         }
+        catch (Exception ex)
+        {
+            // Handle any exceptions here
+            extractedText.Append("Error: " + ex.Message);
+        }
 
-        return $"{files.Count} file(s) uploaded successfully!";
+        return extractedText.ToString();
     }
-    catch (Exception ex)
-    {
-        return $"Exception occurred ! - {ex.Message}";
-    }
-}
 
 }
